e254bd0d7b3f06229db6699d3998b71b5e062bee
mmomir <momirovic.milos@mail.com>
2017-05-02 13:05:39 +0200

    add test

--- test_1.py
+++ test_1.py
@@ -8,3 +8,19 @@ def test_get_tweets():
     Storage._tweets.clear()
     Storage._tweets.append(12)
     assert Storage.get_tweets() == [12]
+
+def test_get_tweet():
+    Storage._tweets.clear()
+    Storage._tweets.append({"id": 1,"name": "Milos", "tweet":"Test"})
+    assert Storage.get_tweet(1) == {"id": 1,"name": "Milos", "tweet":"Test"}
+
+def test_post_tweet():
+    Storage._tweets.clear()
+    Storage.post_tweet("Test")
+    assert Storage._tweets == [{"id": 1,"name": "Milos", "tweet":"Test"}]
+
+def test_del_tweet():
+    Storage._tweets.clear()
+    Storage._tweets.append({"id": 1, "name": "Milos", "tweet":"Test"})
+    Storage.del_tweet(1)
+    assert Storage._tweets == []
